3e3d679196d2f0c2e61df37c7f095f7354e34de3
Lawrence Woodman <lwoodman@vlifesystems.com>
2017-04-03 06:51:17 +0100

    Rearrange the benchmarks to be neater

--- binaryexpr_test.go
+++ binaryexpr_test.go
@@ -11,74 +11,74 @@ import (
 *************************/
 func BenchmarkOpEql(b *testing.B) {
 	b.StopTimer()
+	cases := []struct {
+		lh   *dlit.Literal
+		rh   *dlit.Literal
+		want *dlit.Literal
+	}{
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5), want: dlit.MustNew(true)},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(7), want: dlit.MustNew(false)},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5.3), want: dlit.MustNew(false)},
+		{lh: dlit.MustNew(5),
+			rh:   dlit.MustNew("fred"),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(5.3), rh: dlit.MustNew(5.3), want: dlit.MustNew(true)},
+		{lh: dlit.MustNew(6.3),
+			rh:   dlit.MustNew(5.3),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(6.3),
+			rh:   dlit.MustNew(5.3),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(6.3), rh: dlit.MustNew(5), want: dlit.MustNew(false)},
+		{lh: dlit.MustNew(6.3),
+			rh:   dlit.MustNew("fred"),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew("fred"),
+			rh:   dlit.MustNew("fred"),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew("fred"),
+			rh:   dlit.MustNew("bob"),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew("fred"),
+			rh:   dlit.MustNew(8),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew("fred"),
+			rh:   dlit.MustNew(8.2),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew(errors.New("an error")),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew(errors.New("another error")),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew(5),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew(5.3),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew("bob"),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew("an error"),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+	}
 	for n := 0; n < b.N; n++ {
-		cases := []struct {
-			lh   *dlit.Literal
-			rh   *dlit.Literal
-			want *dlit.Literal
-		}{
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5), want: dlit.MustNew(true)},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(7), want: dlit.MustNew(false)},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5.3), want: dlit.MustNew(false)},
-			{lh: dlit.MustNew(5),
-				rh:   dlit.MustNew("fred"),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(5.3), rh: dlit.MustNew(5.3), want: dlit.MustNew(true)},
-			{lh: dlit.MustNew(6.3),
-				rh:   dlit.MustNew(5.3),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(6.3),
-				rh:   dlit.MustNew(5.3),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(6.3), rh: dlit.MustNew(5), want: dlit.MustNew(false)},
-			{lh: dlit.MustNew(6.3),
-				rh:   dlit.MustNew("fred"),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew("fred"),
-				rh:   dlit.MustNew("fred"),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew("fred"),
-				rh:   dlit.MustNew("bob"),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew("fred"),
-				rh:   dlit.MustNew(8),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew("fred"),
-				rh:   dlit.MustNew(8.2),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew(errors.New("an error")),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew(errors.New("another error")),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew(5),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew(5.3),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew("bob"),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew("an error"),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-		}
 		for _, c := range cases {
 			b.StartTimer()
 			got := opEql(c.lh, c.rh)
@@ -92,74 +92,74 @@ func BenchmarkOpEql(b *testing.B) {
 
 func BenchmarkOpNeq(b *testing.B) {
 	b.StopTimer()
+	cases := []struct {
+		lh   *dlit.Literal
+		rh   *dlit.Literal
+		want *dlit.Literal
+	}{
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5), want: dlit.MustNew(false)},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(7), want: dlit.MustNew(true)},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5.3), want: dlit.MustNew(true)},
+		{lh: dlit.MustNew(5),
+			rh:   dlit.MustNew("fred"),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5.3), rh: dlit.MustNew(5.3), want: dlit.MustNew(false)},
+		{lh: dlit.MustNew(6.3),
+			rh:   dlit.MustNew(5.3),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(6.3),
+			rh:   dlit.MustNew(5.3),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(6.3), rh: dlit.MustNew(5), want: dlit.MustNew(true)},
+		{lh: dlit.MustNew(6.3),
+			rh:   dlit.MustNew("fred"),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew("fred"),
+			rh:   dlit.MustNew("fred"),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew("fred"),
+			rh:   dlit.MustNew("bob"),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew("fred"),
+			rh:   dlit.MustNew(8),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew("fred"),
+			rh:   dlit.MustNew(8.2),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew(errors.New("an error")),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew(errors.New("another error")),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew(5),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew(5.3),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew("bob"),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(errors.New("an error")),
+			rh:   dlit.MustNew("an error"),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+	}
 	for n := 0; n < b.N; n++ {
-		cases := []struct {
-			lh   *dlit.Literal
-			rh   *dlit.Literal
-			want *dlit.Literal
-		}{
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5), want: dlit.MustNew(false)},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(7), want: dlit.MustNew(true)},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5.3), want: dlit.MustNew(true)},
-			{lh: dlit.MustNew(5),
-				rh:   dlit.MustNew("fred"),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5.3), rh: dlit.MustNew(5.3), want: dlit.MustNew(false)},
-			{lh: dlit.MustNew(6.3),
-				rh:   dlit.MustNew(5.3),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(6.3),
-				rh:   dlit.MustNew(5.3),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(6.3), rh: dlit.MustNew(5), want: dlit.MustNew(true)},
-			{lh: dlit.MustNew(6.3),
-				rh:   dlit.MustNew("fred"),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew("fred"),
-				rh:   dlit.MustNew("fred"),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew("fred"),
-				rh:   dlit.MustNew("bob"),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew("fred"),
-				rh:   dlit.MustNew(8),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew("fred"),
-				rh:   dlit.MustNew(8.2),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew(errors.New("an error")),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew(errors.New("another error")),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew(5),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew(5.3),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew("bob"),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(errors.New("an error")),
-				rh:   dlit.MustNew("an error"),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-		}
 		for _, c := range cases {
 			b.StartTimer()
 			got := opNeq(c.lh, c.rh)
@@ -173,31 +173,31 @@ func BenchmarkOpNeq(b *testing.B) {
 
 func BenchmarkOpLand(b *testing.B) {
 	b.StopTimer()
+	cases := []struct {
+		lh   *dlit.Literal
+		rh   *dlit.Literal
+		want *dlit.Literal
+	}{
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(true), rh: dlit.MustNew(true),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(false), rh: dlit.MustNew(true),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(false), rh: dlit.MustNew(false),
+			want: dlit.MustNew(false),
+		},
+	}
 	for n := 0; n < b.N; n++ {
-		cases := []struct {
-			lh   *dlit.Literal
-			rh   *dlit.Literal
-			want *dlit.Literal
-		}{
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(true), rh: dlit.MustNew(true),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(false), rh: dlit.MustNew(true),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(false), rh: dlit.MustNew(false),
-				want: dlit.MustNew(false),
-			},
-		}
 		for _, c := range cases {
 			b.StartTimer()
 			got := opLand(c.lh, c.rh)
@@ -211,34 +211,34 @@ func BenchmarkOpLand(b *testing.B) {
 
 func BenchmarkOpLor(b *testing.B) {
 	b.StopTimer()
+	cases := []struct {
+		lh   *dlit.Literal
+		rh   *dlit.Literal
+		want *dlit.Literal
+	}{
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(true), rh: dlit.MustNew(true),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(false), rh: dlit.MustNew(true),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(true), rh: dlit.MustNew(false),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(false), rh: dlit.MustNew(false),
+			want: dlit.MustNew(false),
+		},
+	}
 	for n := 0; n < b.N; n++ {
-		cases := []struct {
-			lh   *dlit.Literal
-			rh   *dlit.Literal
-			want *dlit.Literal
-		}{
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(true), rh: dlit.MustNew(true),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(false), rh: dlit.MustNew(true),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(true), rh: dlit.MustNew(false),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(false), rh: dlit.MustNew(false),
-				want: dlit.MustNew(false),
-			},
-		}
 		for _, c := range cases {
 			b.StartTimer()
 			got := opLor(c.lh, c.rh)
@@ -252,34 +252,34 @@ func BenchmarkOpLor(b *testing.B) {
 
 func BenchmarkOpLss(b *testing.B) {
 	b.StopTimer()
+	cases := []struct {
+		lh   *dlit.Literal
+		rh   *dlit.Literal
+		want *dlit.Literal
+	}{
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(6),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5.5),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(6), rh: dlit.MustNew(5),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+	}
 	for n := 0; n < b.N; n++ {
-		cases := []struct {
-			lh   *dlit.Literal
-			rh   *dlit.Literal
-			want *dlit.Literal
-		}{
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(6),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5.5),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(6), rh: dlit.MustNew(5),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-		}
 		for _, c := range cases {
 			b.StartTimer()
 			got := opLss(c.lh, c.rh)
@@ -293,37 +293,37 @@ func BenchmarkOpLss(b *testing.B) {
 
 func BenchmarkOpLeq(b *testing.B) {
 	b.StopTimer()
+	cases := []struct {
+		lh   *dlit.Literal
+		rh   *dlit.Literal
+		want *dlit.Literal
+	}{
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(6),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5.5),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5.5),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(6), rh: dlit.MustNew(5),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+	}
 	for n := 0; n < b.N; n++ {
-		cases := []struct {
-			lh   *dlit.Literal
-			rh   *dlit.Literal
-			want *dlit.Literal
-		}{
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(6),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5.5),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5.5),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(6), rh: dlit.MustNew(5),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-		}
 		for _, c := range cases {
 			b.StartTimer()
 			got := opLeq(c.lh, c.rh)
@@ -337,40 +337,40 @@ func BenchmarkOpLeq(b *testing.B) {
 
 func BenchmarkOpGtr(b *testing.B) {
 	b.StopTimer()
+	cases := []struct {
+		lh   *dlit.Literal
+		rh   *dlit.Literal
+		want *dlit.Literal
+	}{
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(6),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5.5),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5.5),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(6), rh: dlit.MustNew(5),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5.1),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+	}
 	for n := 0; n < b.N; n++ {
-		cases := []struct {
-			lh   *dlit.Literal
-			rh   *dlit.Literal
-			want *dlit.Literal
-		}{
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(6),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5.5),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5.5),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(6), rh: dlit.MustNew(5),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5.1),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-		}
 		for _, c := range cases {
 			b.StartTimer()
 			got := opGtr(c.lh, c.rh)
@@ -384,40 +384,40 @@ func BenchmarkOpGtr(b *testing.B) {
 
 func BenchmarkOpGeq(b *testing.B) {
 	b.StopTimer()
+	cases := []struct {
+		lh   *dlit.Literal
+		rh   *dlit.Literal
+		want *dlit.Literal
+	}{
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(6),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5.5),
+			want: dlit.MustNew(false),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5.5),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(6), rh: dlit.MustNew(5),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5.1),
+			want: dlit.MustNew(true),
+		},
+		{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+		{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
+			want: dlit.MustNew(ErrIncompatibleTypes),
+		},
+	}
 	for n := 0; n < b.N; n++ {
-		cases := []struct {
-			lh   *dlit.Literal
-			rh   *dlit.Literal
-			want *dlit.Literal
-		}{
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(6),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5.5),
-				want: dlit.MustNew(false),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(5),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5.5),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(6), rh: dlit.MustNew(5),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(5.5), rh: dlit.MustNew(5.1),
-				want: dlit.MustNew(true),
-			},
-			{lh: dlit.MustNew(true), rh: dlit.MustNew(5),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-			{lh: dlit.MustNew(5), rh: dlit.MustNew(true),
-				want: dlit.MustNew(ErrIncompatibleTypes),
-			},
-		}
 		for _, c := range cases {
 			b.StartTimer()
 			got := opGeq(c.lh, c.rh)
